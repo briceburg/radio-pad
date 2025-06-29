781e56b01a04d3e99a999fa28067f01e19da078a
Brice Burgess <nesta@iceburg.net>
2025-06-29 11:01:11 -0600

    rewrite: single control char, support pages

--- src/main.py
+++ src/main.py
@@ -1,58 +1,26 @@
-import os
 import time
+import json
 import displayio
 import terminalio
 from adafruit_display_shapes.rect import Rect
 from adafruit_display_text import label
 from adafruit_macropad import MacroPad
 from adafruit_hid.keycode import Keycode
-import time
 
-MACRO_FOLDER = "/macros"
+DEFAULT_KEY_COLOR = 0x000077
+HIGHLIGHT_COLOR = 0x015C01
+LED_BRIGHTNESS = 0.10  # Dim the LEDs, 1 is full brightness
+MACROPAD_KEY_COUNT = 12  # Number of keys on the MacroPad
+RADIO_STATIONS_FILE = "stations.json"
 
-
-class App:
-    """Class representing a host-side application, for which we have a set
-    of macro sequences. Project code was originally more complex and
-    this was helpful, but maybe it's excessive now?"""
-
-    def __init__(self, appdata):
-        self.name = appdata["name"]
-        self.macros = appdata["macros"]
-
-    def switch(self):
-        """Activate application settings; update OLED labels and LED
-        colors."""
-        group[13].text = self.name  # Application name
-        if self.name:
-            rect.fill = 0xFFFFFF
-        else:  # empty app name indicates blank screen for which we dimm header
-            rect.fill = 0x000000
-        for i in range(12):
-            if i < len(self.macros):  # Key in use, set label + LED color
-                macropad.pixels[i] = self.macros[i][0]
-                group[i].text = self.macros[i][1]
-            else:  # Key not in use, no label or LED
-                macropad.pixels[i] = 0
-                group[i].text = ""
-        macropad.keyboard.release_all()
-        macropad.consumer_control.release()
-        macropad.mouse.release_all()
-        macropad.stop_tone()
-        macropad.pixels.show()
-        macropad.display.refresh()
-
-
-# INITIALIZATION -----------------------
-
+# Display Setup -----------------------
+# each group displays the station 'name' corresponding to the key number.
 macropad = MacroPad()
 macropad.display.auto_refresh = False
 macropad.pixels.auto_write = False
-macropad.pixels.brightness = 0.10  # Dim the LEDs
-
-# Set up displayio group with all the labels
+macropad.pixels.brightness = LED_BRIGHTNESS
 group = displayio.Group()
-for key_index in range(12):
+for key_index in range(MACROPAD_KEY_COUNT):
     x = key_index % 3
     y = key_index // 3
     group.append(
@@ -80,183 +48,176 @@ group.append(
 )
 macropad.display.root_group = group
 
-# Load all the macro key setups from .py files in MACRO_FOLDER
-apps = []
-files = os.listdir(MACRO_FOLDER)
-files.sort()
-for filename in files:
-    if filename.endswith(".py") and not filename.startswith("._"):
-        try:
-            module = __import__(MACRO_FOLDER + "/" + filename[:-3])
-            apps.append(App(module.app))
-        except (
-            SyntaxError,
-            ImportError,
-            AttributeError,
-            KeyError,
-            NameError,
-            IndexError,
-            TypeError,
-        ) as err:
-            print("ERROR in", filename)
-            import traceback
 
-            traceback.print_exception(err, err, err.__traceback__)
+# INITIALIZATION -----------------------
+class App:
+    def __init__(self, appdata):
+        self.stations = appdata["stations"]
+        self.title = appdata["title"]
+
+    def switch(self):
+        group[13].text = self.title
+        rect.fill = 0xFFFFFF
+        for i in range(MACROPAD_KEY_COUNT):
+            if i < len(self.stations):
+                macropad.pixels[i] = self.stations[i].get("color", DEFAULT_KEY_COLOR)
+                group[i].text = self.stations[i].get("name", "?")
+            else:  # no station assigned to this key, disable LED and label.
+                macropad.pixels[i] = 0
+                group[i].text = ""
+
+        macropad.keyboard.release_all()
+        macropad.consumer_control.release()
+        macropad.mouse.release_all()
+        macropad.stop_tone()
+        macropad.pixels.show()
+        macropad.display.refresh()
+
+
+# add an application for each page of stations, page length is MACROPAD_KEY_COUNT
+apps = []
+with open(RADIO_STATIONS_FILE, "r") as f:
+    stations_list = json.load(f)
+    for i in range(0, len(stations_list), MACROPAD_KEY_COUNT):
+        apps.append(
+            App(
+                {
+                    "stations": stations_list[i : i + MACROPAD_KEY_COUNT],
+                    "title": (
+                        "iCEBURG Radio"
+                        if len(stations_list) <= MACROPAD_KEY_COUNT
+                        else f"iCEBURG Radio {int(i / MACROPAD_KEY_COUNT) + 1}"
+                    ),
+                }
+            )
+        )
+    del stations_list
 
 if not apps:
-    group[13].text = "NO MACRO FILES FOUND"
+    group[13].text = "NO STATIONS DEFINED"
     macropad.display.refresh()
     while True:
         pass
 
-pressed_color = 0x015C01
 last_position = None
 last_pressed = None
 last_encoder_switch = macropad.encoder_switch_debounced.pressed
 app_index = 0
 apps[app_index].switch()
 
-def radio_control(key):
-    """Send a control command follwed by key to the radio-pad application."""
+
+def radio_control(cmd_char, arg_char):
+    """Sends a command and argument to the bin/radio-pad listener. Escape sequence is Ctrl+@"""
     macropad.keyboard.press(Keycode.CONTROL)
-    macropad.keyboard.press(Keycode.SIX)
+    macropad.keyboard.press(Keycode.TWO)
     macropad.keyboard.release(Keycode.CONTROL)
-    macropad.keyboard.release(Keycode.SIX)
-    macropad.keyboard.press(key)
-    macropad.keyboard.release(key)
+    macropad.keyboard.release(Keycode.TWO)
+    macropad.keyboard_layout.write(cmd_char)
+    macropad.keyboard_layout.write(arg_char)
+
+
+def index_to_char(idx):
+    """
+    Convert an integer index to a single character.
+    Allows up to 35 pages of stations to be represented by a single keypress.
+    0-9 -> '0'-'9', 10-35 -> 'a'-'z'.
+    """
+    if 0 <= idx <= 9:
+        return str(idx)
+    elif 10 <= idx <= 35:
+        return chr(ord("a") + (idx - 10))
+    else:
+        raise ValueError("Index out of supported range (0-35)")
+
 
 # MAIN LOOP ----------------------------
-
 while True:
-    # Read encoder position. If it's changed, adjust volume.
+    # Read encoder position.
     position = macropad.encoder
     if last_position is not None and position != last_position:
-        radio_control(
-            Keycode.UP_ARROW if position > last_position else Keycode.DOWN_ARROW
-        )
+        if last_pressed is not None:
+            # if a station is playing, change volume
+            radio_control("V", "+" if position > last_position else "-")
+        else:
+
+            # else, change station page
+            # we cannot use app_index = position % len(apps) -- because the encoder value is linked to volume as well
+            # TODO: investiagate ability to reset the macropad.encoder value when switching apps
+            if position > last_position:
+                app_index += 1
+                if app_index > len(apps) - 1:
+                    app_index = 0
+            else:
+                app_index -= 1
+                if app_index < 0:
+                    app_index = len(apps) - 1
+            apps[app_index].switch()
+
     last_position = position
 
-    # Handle encoder button. If it's pressed, stop radio. 
+    # Handle encoder button. If it's pressed, stop radio.
     macropad.encoder_switch_debounced.update()
     encoder_switch = macropad.encoder_switch_debounced.pressed
     if encoder_switch != last_encoder_switch:
         last_encoder_switch = encoder_switch
         if last_pressed is not None:
             # stop radio
-            radio_control(Keycode.LEFT_ARROW)
+            radio_control("X", "*")
 
             # reset radio display and highlighted key to its original
             group[last_pressed].color = 0xFFFFFF
             group[last_pressed].background_color = 0x000000
-            group[13].text = apps[app_index].name
+            group[13].text = apps[app_index].title
             macropad.display.refresh()
+
             last_pressed = None
 
             # flash the pixels
-            for i in range(12):
+            for i in range(MACROPAD_KEY_COUNT):
                 macropad.pixels[i] = 0x990909
             macropad.pixels.show()
             time.sleep(0.66)
 
             # restore the pixels
-            for i in range(12):
+            for i in range(MACROPAD_KEY_COUNT):
                 try:
-                    macropad.pixels[i] = apps[app_index].macros[i][0]
+                    macropad.pixels[i] = (
+                        apps[app_index].stations[i].get("color", DEFAULT_KEY_COLOR)
+                    )
                 except IndexError:
                     macropad.pixels[i] = 0
             macropad.pixels.show()
-            
+
         continue
     else:
         event = macropad.keys.events.get()
-        if not event or event.key_number >= len(apps[app_index].macros):
-            continue  # No key events, or no corresponding macro, resume loop
+        if not event or event.key_number >= len(apps[app_index].stations):
+            continue  # No key events, or no corresponding station, resume loop
         key_number = event.key_number
         pressed = event.pressed
 
-    # If code reaches here, a key WAS pressed/released and there's a corresponding macro.
-    sequence = apps[app_index].macros[key_number][2]
-    if pressed:
-        # 'sequence' is an arbitrary-length list, each item is one of:
-        # Positive integer (e.g. Keycode.KEYPAD_MINUS): key pressed
-        # Negative integer: (absolute value) key released
-        # Float (e.g. 0.25): delay in seconds
-        # String (e.g. "Foo"): corresponding keys pressed & released
-        # List []: one or more Consumer Control codes (can also do float delay)
-        # Dict {}: mouse buttons/motion (might extend in future)
-        if key_number < 12:  # No pixel for encoder button
-            macropad.pixels[key_number] = pressed_color
-            macropad.pixels.show()
+    # If code reaches here, a key WAS pressed/released and there's a corresponding station.
+    if pressed and key_number < MACROPAD_KEY_COUNT:  # No pixel for encoder button
 
-            # highlight the station pressed on the OLED display
-            if last_pressed != key_number:
-                group[key_number].color = 0x000000
-                group[key_number].background_color = 0xFFFFFF
-                group[13].text = apps[app_index].macros[key_number][1]
+        # highlight the key and label
+        macropad.pixels[key_number] = HIGHLIGHT_COLOR
+        group[key_number].color = 0x000000
+        group[key_number].background_color = 0xFFFFFF
+        group[13].text = apps[app_index].stations[key_number].get("name", "?")
 
-                # reset highlight on the previously selected station
-                if last_pressed is not None:
-                    group[last_pressed].color = 0xFFFFFF
-                    group[last_pressed].background_color = 0x000000
+        # remove highlighting from previous key and label
+        if last_pressed is not None and last_pressed != key_number:
+            macropad.pixels[last_pressed] = (
+                apps[app_index].stations[last_pressed].get("color", DEFAULT_KEY_COLOR)
+            )
+            group[last_pressed].color = 0xFFFFFF
+            group[last_pressed].background_color = 0x000000
 
-                macropad.display.refresh()
-        for item in sequence:
-            if isinstance(item, int):
-                if item >= 0:
-                    macropad.keyboard.press(item)
-                else:
-                    macropad.keyboard.release(-item)
-            elif isinstance(item, float):
-                time.sleep(item)
-            elif isinstance(item, str):
-                macropad.keyboard_layout.write(item)
-            elif isinstance(item, list):
-                for code in item:
-                    if isinstance(code, int):
-                        macropad.consumer_control.release()
-                        macropad.consumer_control.press(code)
-                    if isinstance(code, float):
-                        time.sleep(code)
-            elif isinstance(item, dict):
-                if "buttons" in item:
-                    if item["buttons"] >= 0:
-                        macropad.mouse.press(item["buttons"])
-                    else:
-                        macropad.mouse.release(-item["buttons"])
-                macropad.mouse.move(
-                    item["x"] if "x" in item else 0,
-                    item["y"] if "y" in item else 0,
-                    item["wheel"] if "wheel" in item else 0,
-                )
-                if "tone" in item:
-                    if item["tone"] > 0:
-                        macropad.stop_tone()
-                        macropad.start_tone(item["tone"])
-                    else:
-                        macropad.stop_tone()
-                elif "play" in item:
-                    macropad.play_file(item["play"])
+        # play station
+        radio_control(index_to_char(app_index), index_to_char(key_number))
+
+        macropad.pixels.show()
+        macropad.display.refresh()
+        last_pressed = key_number
     else:
-        # Release any still-pressed keys, consumer codes, mouse buttons
-        # Keys and mouse buttons are individually released this way (rather
-        # than release_all()) because pad supports multi-key rollover, e.g.
-        # could have a meta key or right-mouse held down by one macro and
-        # press/release keys/buttons with others. Navigate popups, etc.
-        for item in sequence:
-            if isinstance(item, int):
-                if item >= 0:
-                    macropad.keyboard.release(item)
-            elif isinstance(item, dict):
-                if "buttons" in item:
-                    if item["buttons"] >= 0:
-                        macropad.mouse.release(item["buttons"])
-                elif "tone" in item:
-                    macropad.stop_tone()
         macropad.consumer_control.release()
-
-        if key_number < 12:  # No pixel for encoder button
-            # reset previously highlighted key to its original color
-            if last_pressed is not None and last_pressed != key_number:
-                macropad.pixels[last_pressed] = apps[app_index].macros[last_pressed][0]
-                macropad.pixels.show()
-            last_pressed = key_number
