4ecd47f8bd07405a33b63de3d24af828b9beebb4
Brice Burgess <nesta@iceburg.net>
2025-07-28 20:51:01 -0600

    remote-control: move lint config to package.json

--- remote-control/src/js/lib/switchboard.js
+++ remote-control/src/js/lib/switchboard.js
@@ -1,5 +1,3 @@
-import { EventEmitter } from "./interfaces.js";
-
 /*
 This file is part of the radio-pad project.
 https://github.com/briceburg/radio-pad
@@ -17,6 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
+import { EventEmitter } from "./interfaces.js";
 
 export class RadioPadSwitchboard extends EventEmitter {
   constructor() {
