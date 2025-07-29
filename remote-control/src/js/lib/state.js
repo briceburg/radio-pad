b5a5c55fcfc49f3f1ab262e32da00b86b727221e
Brice Burgess <nesta@iceburg.net>
2025-07-28 20:10:55 -0600

    remote-control: let the events drive :car:

---
+++ remote-control/src/js/lib/state.js
@@ -0,0 +1,46 @@
+/*
+This file is part of the radio-pad project.
+https://github.com/briceburg/radio-pad
+
+Copyright (c) 2025 Brice Burgess <https://github.com/briceburg>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { EventEmitter } from './interfaces.js';
+
+export class RadioPadState extends EventEmitter {
+  constructor(initialState = {
+        player: {
+          id: null,
+          name: null,
+          stationsUrl: null,
+          switchboardUrl: null
+        },
+        stationsUrl: null,
+        currentStation: null
+      }) {
+    super();
+    this.state = initialState;
+  }
+
+  get(key) {
+    return this.state[key] || null;
+  }
+
+  async set(key, value) {
+    if (value !== this.state[key]) {
+      this.state[key] = value;
+      await this.emitEvent('on-change', { key, value });
+    }
+  }
+}
